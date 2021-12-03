98987c4ec44e483391eaf74fc50ad338fc2c7819
Subhrajyotimodak <writer1ani@gmail.com>
2021-12-03 15:54:53 +0530

    heading

--- dist/components/handler/headingHandlerFn.js
+++ dist/components/handler/headingHandlerFn.js
@@ -6,21 +6,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.HeadingArray = void 0;
 var react_1 = __importDefault(require("react"));
 var react_dom_1 = require("react-dom");
+var uuid_1 = require("../../utils/uuid");
 function headingHandlerFn(_a) {
     var editorState = _a.editorState, onBack = _a.onBack, name = _a.name;
     function onClick(e) {
+        var _a, _b;
         var element = e.target;
         if (!element)
             return;
         var value = element.id;
+        var tag = "<".concat(value, ">");
         console.log(value);
-        editorState.__document__.execCommand("formatBlock", false, value);
+        editorState.__document__.execCommand("formatBlock", false, tag);
+        (_b = (_a = editorState.editor) === null || _a === void 0 ? void 0 : _a.querySelector("".concat(value, ":not([id])"))) === null || _b === void 0 ? void 0 : _b.setAttribute("id", (0, uuid_1.uuid)());
     }
     (0, react_dom_1.render)(react_1.default.createElement("div", { id: "subMenu" + name, className: "subMenuWrapper" },
         react_1.default.createElement("div", { className: "subMenuHeading" },
             react_1.default.createElement("button", { onClick: function () { return onBack(document.getElementById("subMenu" + name)); } }, "Back"),
             react_1.default.createElement("span", null, name)),
-        react_1.default.createElement("div", { className: "subMenuExpanded" }, exports.HeadingArray.map(function (size, value) { return (react_1.default.createElement("button", { className: "modelBtn", id: "<h".concat(value + 1, ">"), key: value + size, onClick: onClick, style: {
+        react_1.default.createElement("div", { className: "subMenuExpanded" }, exports.HeadingArray.map(function (size, value) { return (react_1.default.createElement("button", { className: "modelBtn", id: "h".concat(value + 1), key: value + size, onClick: onClick, style: {
                 fontWeight: "bolder",
                 fontSize: "".concat(size, "px"),
             } }, "Heading ".concat(value + 1))); }))), document.getElementById("expanded"));
--- src/components/handler/headingHandlerFn.tsx
+++ src/components/handler/headingHandlerFn.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { HandlerFnProps } from "../../base/base.types";
+import { uuid } from "../../utils/uuid";
 
 export default function headingHandlerFn({
   editorState,
@@ -12,8 +13,13 @@ export default function headingHandlerFn({
     if (!element) return;
 
     const value = element.id;
+    const tag = `<${value}>`;
     console.log(value);
-    editorState.__document__.execCommand("formatBlock", false, value);
+
+    editorState.__document__.execCommand("formatBlock", false, tag);
+    editorState.editor
+      ?.querySelector(`${value}:not([id])`)
+      ?.setAttribute("id", uuid());
   }
 
   render(
@@ -30,7 +36,7 @@ export default function headingHandlerFn({
         {HeadingArray.map((size, value) => (
           <button
             className="modelBtn"
-            id={`<h${value + 1}>`}
+            id={`h${value + 1}`}
             key={value + size}
             onClick={onClick}
             style={{
